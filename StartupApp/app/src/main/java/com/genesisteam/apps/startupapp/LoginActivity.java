61c3cfe93707bbd45ca8a04ab21c5c18266906e9
josef <androiment@gmail.com>
2015-12-30 16:35:19 -0600

    lesson minor changes

--- StartupApp/app/src/main/java/com/genesisteam/apps/startupapp/LoginActivity.java
+++ StartupApp/app/src/main/java/com/genesisteam/apps/startupapp/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
